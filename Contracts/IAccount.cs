8966073142db27dabf371d3b87cacfeea16d48f7
Jean Carlos <jcpds14@gmail.com>
2022-10-21 18:51:58 -0300

    move IAccount to Contracts folder and install packages of ENtity Framework and SqlServer

--- Contracts/IAccount.cs
+++ Contracts/IAccount.cs
@@ -2,8 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNet.Models;
 
-namespace DotNet.Models
+namespace DotNet.Contracts
 {
     public interface IAccount
     {
